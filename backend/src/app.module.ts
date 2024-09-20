f21ca3de2f36a55d080fbf9b8b94c8096d9c673e
Przemyslaw Krepski <krepski80@gmail.com>
2024-09-20 22:34:42 +0200

    users route created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ServiceModule } from './service/service.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ServiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
